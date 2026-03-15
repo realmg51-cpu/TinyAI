100a74cc8b3dc65bd43c0f1621e8a6d4976dff65
RealMG <realmg51@gmail.com>
2026-03-15 10:27:20 +0700

    Update and rename Main.cs to TinyAI.Main.cs

---
+++ src/Main/TinyAI.Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TinyAI
+{
+    class Program
+    {
+        static void Main()
+        {
+            ChatCore.Welcome();  // Show welcome message
+            ChatCore.Logic();    // Start chat logic
+        }
+    }
+}
